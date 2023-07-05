311f222cfe762499ff215f39f1052bcf64ca2740
pedro558 <pedroafonso109@gmail.com>
2023-07-05 08:06:34 -0300

    :recycle: refactor: using dependency inversion principle

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,9 @@
 const AppError = require("../utils/AppError")
-const sqliteConnection = require('../database/sqlite')
 const { hash, compare } = require('bcrypt')
+const UserRepository = require('../repositories/UserRepository')
 
+const sqliteConnection = require('../database/sqlite')
+const UserCreateService = require('../services/UserCreateService')
 
 class UsersController {
   async index(req, res){
@@ -12,23 +14,11 @@ class UsersController {
 
   async create(req, res){
     const { name, email, password } = req.body
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
 
-    const database = await sqliteConnection()
-    const checkUserExists = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    )
-
-    if(checkUserExists){
-      throw new AppError("Este e-mail já está em uso")
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]
-    )
+    await userCreateService.execute({ name, email, password })
+    
 
     return res.status(201).json()
    
